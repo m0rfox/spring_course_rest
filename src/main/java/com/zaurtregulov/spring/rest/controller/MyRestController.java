615a9682a759496f3b6a6e1661493297b0fdaf8b
Valerii Glavatskih <glavatskihvalerii@yandex.ru>
2021-12-21 22:55:53 +0400

    Lesson 86

--- src/main/java/com/zaurtregulov/spring/rest/controller/MyRestController.java
+++ src/main/java/com/zaurtregulov/spring/rest/controller/MyRestController.java
@@ -46,5 +46,12 @@ public class MyRestController {
         return employee;
     }
 
+    @PutMapping("/employees")
+    public Employee updateEmployee (@RequestBody Employee employee){
+
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+
 
 }
